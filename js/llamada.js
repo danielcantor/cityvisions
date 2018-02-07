d912d60c6a14b5a296b847ef5dcabc0cf3da6b4e
Daniel Cantor <daniel.cantor2019@gmail.com>
2018-02-07 09:51:22 -0400

    Cambios para el modo automatico

--- js/llamada.js
+++ js/llamada.js
@@ -197,8 +197,7 @@ $('document').ready(function(){
         });
     });
     // modo automatico
-    $('#activar').click(function(){
-        if(horario == 1){
+    $('#activar1').click(function(){
             var hora = $('#hora1').val();
             var minuto = $('#min1').val();
             var segundo = $('#seg1').val();
@@ -208,7 +207,7 @@ $('document').ready(function(){
                 type: 'post',
                 url: 'inc/automatico.php',
                 data: {
-                    'horario': horario,
+                    'horario': 1,
                     'hora': hora,
                     'minuto': minuto,
                     'segundo': segundo,
@@ -226,94 +225,95 @@ $('document').ready(function(){
                     }, 6000);
                 }
             });
-        }
-        if (horario == 2) {
-            var hora = $('#hora1').val();
-            var minuto = $('#min1').val();
-            var segundo = $('#seg1').val();
-            var zona = $('#hor1').val();
-            var duracion = $('#ciclo1').val();
-            var hora2 = $('#hora2').val();
-            var minuto2 = $('#min2').val();
-            var segundo2 = $('#seg2').val();
-            var zona2 = $('#hor2').val();
-            var duracion2 = $('#ciclo2').val();
-            $.ajax({
-                type: 'post',
-                url: 'inc/automatico.php',
-                data: {
-                    'horario': horario,
-                    'hora': hora,
-                    'minuto': minuto,
-                    'segundo': segundo,
-                    'zona': zona,
-                    'duracion': duracion,
-                    'hora2': hora2,
-                    'minuto2': minuto2,
-                    'segundo2': segundo2,
-                    'zona2': zona2,
-                    'duracion2': duracion2
-                }, success: function (w) {
-                    $('#resp-down').removeClass('d-none');
-                    $.post("inc/modo.php", function (htmlexterno) {
-                        $("#modo").html(htmlexterno);
-                    });
-                    $("#respuesta").html(w);
-                    setTimeout(function () {
-                        $('#respuesta').alert('close');
-                    }, 6000);
-                }
-            });
-        }
-        if (horario == 3) {
-            var hora = $('#hora1').val();
-            var minuto = $('#min1').val();
-            var segundo = $('#seg1').val();
-            var zona = $('#hor1').val();
-            var duracion = $('#ciclo1').val();
-            var hora2 = $('#hora2').val();
-            var minuto2 = $('#min2').val();
-            var segundo2 = $('#seg2').val();
-            var zona2 = $('#hor2').val();
-            var duracion2 = $('#ciclo2').val();
-            var hora3 = $('#hora3').val();
-            var minuto3 = $('#min3').val();
-            var segundo3 = $('#seg3').val();
-            var zona3 = $('#hor3').val();
-            var duracion3 = $('#ciclo3').val();
-            $.ajax({
-                type: 'post',
-                url: 'inc/automatico.php',
-                data: {
-                    'hora': hora,
-                    'minuto': minuto,
-                    'segundo': segundo,
-                    'zona': zona,
-                    'duracion': duracion,
-                    'hora2': hora2,
-                    'minuto2': minuto2,
-                    'segundo2': segundo2,
-                    'zona2': zona2,
-                    'duracion2': duracion2,
-                    'hora3': hora3,
-                    'minuto3': minuto3,
-                    'segundo3': segundo3,
-                    'zona3': zona3,
-                    'duracion3': duracion3
-                }, success: function (w) {
-                    $('#resp-down').removeClass('d-none');
-                    $.post("inc/modo.php", function (htmlexterno) {
-                        $("#modo").html(htmlexterno);
-                    });
-                    $("#respuesta").html(w);
-                    setTimeout(function () {
-                        $('#respuesta').alert('close');
-                    }, 6000);
-                }
-            });
-        }
-
     });
+
+    $('#activar2').click(function () {
+        var hora = $('#hora1').val();
+        var minuto = $('#min1').val();
+        var segundo = $('#seg1').val();
+        var zona = $('#hor1').val();
+        var duracion = $('#ciclo1').val();
+        var hora2 = $('#hora2').val();
+        var minuto2 = $('#min2').val();
+        var segundo2 = $('#seg2').val();
+        var zona2 = $('#hor2').val();
+        var duracion2 = $('#ciclo2').val();
+        $.ajax({
+            type: 'post',
+            url: 'inc/automatico.php',
+            data: {
+                'horario': 2,
+                'hora': hora,
+                'minuto': minuto,
+                'segundo': segundo,
+                'zona': zona,
+                'duracion': duracion,
+                'hora2': hora2,
+                'minuto2': minuto2,
+                'segundo2': segundo2,
+                'zona2': zona2,
+                'duracion2': duracion2
+            }, success: function (w) {
+                $('#resp-down').removeClass('d-none');
+                $.post("inc/modo.php", function (htmlexterno) {
+                    $("#modo").html(htmlexterno);
+                });
+                $("#respuesta").html(w);
+                setTimeout(function () {
+                    $('#respuesta').alert('close');
+                }, 6000);
+            }
+        });
+    });
+    $('#activar3').click(function(){
+        var hora = $('#hora1').val();
+        var minuto = $('#min1').val();
+        var segundo = $('#seg1').val();
+        var zona = $('#hor1').val();
+        var duracion = $('#ciclo1').val();
+        var hora2 = $('#hora2').val();
+        var minuto2 = $('#min2').val();
+        var segundo2 = $('#seg2').val();
+        var zona2 = $('#hor2').val();
+        var duracion2 = $('#ciclo2').val();
+        var hora3 = $('#hora3').val();
+        var minuto3 = $('#min3').val();
+        var segundo3 = $('#seg3').val();
+        var zona3 = $('#hor3').val();
+        var duracion3 = $('#ciclo3').val();
+        $.ajax({
+            type: 'post',
+            url: 'inc/automatico.php',
+            data: {
+                'horario': 3,
+                'hora': hora,
+                'minuto': minuto,
+                'segundo': segundo,
+                'zona': zona,
+                'duracion': duracion,
+                'hora2': hora2,
+                'minuto2': minuto2,
+                'segundo2': segundo2,
+                'zona2': zona2,
+                'duracion2': duracion2,
+                'hora3': hora3,
+                'minuto3': minuto3,
+                'segundo3': segundo3,
+                'zona3': zona3,
+                'duracion3': duracion3
+            }, success: function (w) {
+                $('#resp-down').removeClass('d-none');
+                $.post("inc/modo.php", function (htmlexterno) {
+                    $("#modo").html(htmlexterno);
+                });
+                $("#respuesta").html(w);
+                setTimeout(function () {
+                    $('#respuesta').alert('close');
+                }, 6000);
+            }
+        });
+    });
+       
     //logica de checkbox en modo programado
     $('.col-12 .zone *:checkbox').click(function(){
         var check=$(this).attr('class');
